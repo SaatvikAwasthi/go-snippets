ee7b8ead93155936eed9a3f7ef55a0c96a3951d4
saatvik <saatvikawasthi1998@gmail.com>
2023-10-17 15:46:00 +0530

    Initial Commit

---
+++ cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"strconv"
+)
+
+const (
+	port            = "port"
+	portUsage       = "use to specify port"
+	defaultPort int = 8080
+)
+
+var httpPort string
+
+func init() {
+	args := flag.Int(port, defaultPort, portUsage)
+	log.Printf("Port: %v", *args)
+	httpPort = strconv.Itoa(*args)
+}
+
+func main() {
+}
